de0831a0450075cb45f6f253cdff2d709fe034e6
Ismat Fatima <159720166+Fatima-Ismat@users.noreply.github.com>
2024-07-05 00:37:04 +0500

    Add files via upload
    its my todolist is done

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("Welcome to Ismat Fatima's To Do list\n");
+let myLoop = true;
+let todoListArray: string[] = [];
+
+while (myLoop) {
+  const userInput = await inquirer.prompt([
+    {
+      type: "input",
+      name: "todoItem",
+      message: "Enter item in you To Do list!",
+    },
+    {
+      type: "confirm",
+      name: "addMore",
+      message: "Do you want to add more items?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "seeList",
+      message: "Do you want to see the list?",
+      default: false,
+      when(userInput) {
+        return userInput.addMore === false;
+      },
+    },
+  ]);
+  const { todoItem, addMore, seeList } = userInput;
+
+  if (todoItem) {
+    todoListArray.push(todoItem);
+  }
+  if (seeList) {
+    console.log(`\nHere is your To Do List:`);
+    todoListArray.forEach((item, index) => {
+      console.log(`${index + 1}. ${item}`);
+    });
+  }
+  myLoop = addMore;
+}
